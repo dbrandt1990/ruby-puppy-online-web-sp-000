c0324d7ed751061b7e070c90a4f9bb77234b5042
Danny Brandt <dvbrandt90@gmail.com>
2021-01-04 18:34:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+require 'pry'
+class Dog 
+  binding.pry
+  attr_reader :name, :all
+  
+  @@all = []
+  
+  def save
+    @@all << self.name
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each {|name| puts name}
+  end
+  
+  def self.clear_all
+    @@all.clear()
+  end
+end
